43b102c713965c9b4b5e9fd19dede7c510131b7b
Ben S. Wallace <bwallace@hsph.harvard.edu>
2023-01-02 23:11:47 -0500

    Initial commit

---
+++ includes/blocks/search-form.php
@@ -0,0 +1,34 @@
+<?php
+
+function gp_search_form_render_cb($atts) {
+    $bgColor = esc_attr($atts['bgColor']);
+    $textColor = esc_attr($atts['textColor']);
+    $styleAttr = "background-color:{$bgColor};color:{$textColor};";
+
+    ob_start();
+    ?>
+        <div style="<?php echo $styleAttr; ?>"
+            class="wp-block-g263-plus-search-form">
+          <h1>
+            <?php esc_html_e('Search', 'g263-plus'); ?>:
+            <?php the_search_query(); ?>
+          </h1>
+          <form action="<?php echo esc_url(home_url('/')); ?>">
+            <input type="text" 
+            placeholder="<?php esc_html_e('Search', 'g263-plus'); ?>" 
+            name="s"
+            value="<?php the_search_query(); ?>" />
+            <div class="btn-wrapper">
+              <button type="submit" style="<?php echo $styleAttr; ?>">
+              <?php esc_html_e('Search', 'g263-plus'); ?>
+            </button>
+            </div>
+          </form>
+        </div>
+    <?php
+
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+}
